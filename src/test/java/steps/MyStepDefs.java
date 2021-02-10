2809d29d76a00c68d2e62b8cd35aa3ee6d5074c9
Shyrmovskyi <PShyrmovskyi@poland.luxoft.com>
2021-02-10 15:37:47 +0100

    adding logback to silence debug logging for httpclient

---
+++ src/test/java/steps/MyStepDefs.java
@@ -0,0 +1,31 @@
+package steps;
+
+import core.selenium.BrowserFactory;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import org.openqa.selenium.WebDriver;
+
+
+public class MyStepDefs {
+
+  WebDriver driver;
+
+  @Before
+  public void setUpDriver() {
+    driver = BrowserFactory.setUpBrowser();
+  }
+
+  @Given("Google page is opened")
+  public void openGooglePage() throws InterruptedException {
+    driver.get("https://www.google.com/");
+    Thread.sleep(5000);
+  }
+
+
+  @After
+  public void tearDown() {
+    driver.manage().deleteAllCookies();
+    driver.quit();
+  }
+}
---
+++ src/test/resources/features/MainPage.feature
@@ -0,0 +1,3 @@
+Feature: Go to Google
+  Scenario: Verify that test can navigate to Google page
+    Given Google page is opened
